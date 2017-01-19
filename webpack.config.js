d608378e28122e95aa0353b05c781d8a5c572579
lei.wang_1 <lei.wang_1@quanshi.com>
2017-01-19 17:13:56 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+'use strict';
+var path = require('path')
+var webpack = require('webpack')
+
+var env = process.env.NODE_ENV
+var config = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: 'dist/',
+    filename: 'build.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]'
+        }
+      },
+      {test: /\.css$/, loader: 'style-loader!css-loader'}
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true
+  },
+  devtool: '#eval-source-map'
+};
+
+if (env === 'production') {
+  config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        warnings: false,
+        screw_ie8: false
+      },
+      mangle: {
+        screw_ie8: false
+      },
+      output: {
+        screw_ie8: false
+      }
+    })
+  )
+}
+
+module.exports = config
